ef766cad3e8b0d5d1b645d4624899c281dd63ed0
pjh7@duke.edu <pjh7@duke.edu>
2015-11-18 11:40:09 -0500

    Pop out windows

--- imagestats.m
+++ imagestats.m
@@ -5,6 +5,12 @@ c = uicontextmenu('UserData',struct('imHandle',imHandle));
 imHandle.UIContextMenu = c;
 uimenu(c,'Label','Draw New ROI','Callback',@drawNewROI);
 uimenu(c,'Label','Clear All ROIs','Callback',@clearAllROI);
+addlistener(imHandle,'CData','PostSet',@notification)
+end
+
+function notification(hObject,eventdata)
+imHandle = eventdata.AffectedObject;
+imHandle.UserData.ROI = updateROI(imHandle.UserData.ROI);
 end
 
 function clearAllROI(hObject,eventdata)
@@ -29,6 +35,9 @@ end
 end
 
 function ROI = updateROI(ROI)
+if isempty(ROI)
+    return
+end
 if length(ROI)>1
     for i = 1:length(ROI);
         ROI(i) = updateROI(ROI(i));
@@ -41,8 +50,11 @@ set(ROI.patchHandle(:),'FaceColor',ColorOrder(mod(index-1,size(ColorOrder,1))+1,
 mask = getROImask(ROI.imHandle,ROI.patchHandle);
 ROI1 = getROIdata(ROI.imHandle,mask);
 fnames = fieldnames(ROI1);
-for i = 1:length(fnames)
+for i = 2:length(fnames)
     ROI.(fnames{i}) = ROI1.(fnames{i});
+end
+if ~isempty(ROI.statTable) && ishandle(ROI.statTable)
+    set(ROI.statTable,'Data',ROI2cell(ROI));
 end
 for i = 1:length(ROI.patchHandle)
     UIMenu = ROI.patchHandle(i).UIContextMenu;
@@ -64,6 +76,23 @@ for i = 1:length(ROI.patchHandle)
     end
     for j = [1:index-1 index+1:length(ROI.imHandle.UserData.ROI)];
        uimenu('Parent',compareMenu,'Label',ROI.imHandle.UserData.ROI(j).Name,'Callback',@compareAgainst,'ForeGroundColor',ROI.imHandle.UserData.ROI(j).patchHandle(1).FaceColor); 
+    end
+end
+imHandle = ROI.imHandle;
+if ~isempty(imHandle.UserData.Comparison)
+    if strcmp(ROI.Name,imHandle.UserData.Comparison.fgROI) || strcmp(ROI.Name,imHandle.UserData.Comparison.bgROI)
+        index0 = find(strcmp({imHandle.UserData.ROI.Name},imHandle.UserData.Comparison.fgROI));
+        index1 = find(strcmp({imHandle.UserData.ROI.Name},imHandle.UserData.Comparison.bgROI));
+        Comparison = getComparison(imHandle,index0,index1);
+        if isfield(imHandle.UserData.Comparison,'compTable')
+            Comparison.compTable = imHandle.UserData.Comparison.compTable;
+            if  ~isempty(Comparison.compTable) && ishandle(Comparison.compTable)
+                set(Comparison.compTable,'Data',comp2cell(Comparison));
+            end
+        else
+            Comparison.compTable = [];
+        end
+        imHandle.UserData.Comparison = Comparison;
     end
 end
 end
@@ -126,7 +155,31 @@ uimenu(c,'Label','Delete','Callback',@deleteROI);
 uimenu(c,'Label','Edit','Callback',@editROI);
 uimenu(c,'Label','Add To ROI','Callback',@addNewROI);
 uimenu(c,'Label','Stats');
+uimenu(c,'Label','Show Stats Window','Callback',@toggleStatsWindow);
 uimenu(c,'Label','Compare Against...');
+end
+
+function toggleStatsWindow(hObject,eventdata)
+imHandle = hObject.Parent.UserData.imHandle;
+axH = ancestor(imHandle,'axes');
+figH = ancestor(imHandle,'figure');
+patchHandle = hObject.Parent.UserData.patchHandle;
+[index] = findROI(imHandle,patchHandle);
+ROI = imHandle.UserData.ROI(index);
+st = ROI.statTable;
+if isempty(st) || any(~ishandle(st))
+    fpos = get(figH,'position');
+    f = figure;
+    set(f,'Position',[fpos(1) fpos(2) 200 170]);
+    set(f,'ToolBar','none','MenuBar','none','NumberTitle','off','Name','Stats','Resize','off')
+    C = ROI2cell(ROI);
+    imHandle.UserData.ROI(index).statTable = uitable('Units','pixels','Position',[0 0 210 170],'ColumnWidth',{80,120},'RowStriping','on','Data',C,'ColumnName',[],'RowName',[],'FontSize',14);
+end
+end
+
+function C = ROI2cell(ROI)
+    C = [fieldnames(ROI) struct2cell(ROI)];
+    C = C(1:6,:);
 end
 
 function compareAgainst(hObject,eventdata)
@@ -134,23 +187,60 @@ imHandle = hObject.Parent.Parent.UserData.imHandle;
 patchHandle = hObject.Parent.Parent.UserData.patchHandle;
 [index0,patchindex0] = findROI(imHandle,patchHandle);
 index1 = find(strcmp({imHandle.UserData.ROI.Name},hObject.Label));
+Comparison = getComparison(imHandle,index0,index1);
+% roidata0 = imHandle.UserData.ROI(index0);
+% roidata1 = imHandle.UserData.ROI(index1);
+% mask0 = getROImask(imHandle,roidata0.patchHandle);
+% mask1 = getROImask(imHandle,roidata1.patchHandle);
+% roidata0 = getROIdata(imHandle,mask0);
+% roidata1 = getROIdata(imHandle,mask1);
+% cdata = imHandle.CData;
+% [H,P,CI] = ttest2(cdata(mask0),cdata(mask1));
+% Comparison.fgROI = imHandle.UserData.ROI(index0).Name;
+% Comparison.bgROI = imHandle.UserData.ROI(index1).Name;
+% Comparison.ratio = roidata0.mean/roidata1.mean;
+% Comparison.contrast = (roidata0.mean-roidata1.mean)/roidata1.mean;
+% Comparison.std_joint = sqrt((roidata0.N*(roidata0.std^2)+roidata1.N*(roidata1.std^2))/(roidata0.N+roidata1.N));
+% Comparison.CNR = abs(roidata0.mean-roidata1.mean)/Comparison.std_joint;
+% Comparison.TTest_H = H;
+% Comparison.TTest_P = P;
+% Comparison.TTest_DiffMeansCI = CI;
+fprintf('Comparison:\n');
+disp(Comparison);
+if ~isfield(imHandle.UserData.Comparison,'compTable') || isempty(imHandle.UserData.Comparison.compTable) || ~ishandle(imHandle.UserData.Comparison.compTable)
+    fpos = get(ancestor(imHandle,'figure'),'position');
+    f = figure;
+    set(f,'Position',[fpos(1)+200 fpos(2) 220 230]);
+    set(f,'ToolBar','none','MenuBar','none','NumberTitle','off','Name','Comparison','Resize','off')
+    C = comp2cell(Comparison);
+    Comparison.compTable = uitable('Units','pixels','Position',[0 0 220 230],'ColumnWidth',{90,120},'RowStriping','on','Data',C,'ColumnName',[],'RowName',[],'FontSize',14);
+end
+imHandle.UserData.Comparison = Comparison;
+end
+
+function Comparison = getComparison(imHandle,index0,index1);
 roidata0 = imHandle.UserData.ROI(index0);
 roidata1 = imHandle.UserData.ROI(index1);
 mask0 = getROImask(imHandle,roidata0.patchHandle);
 mask1 = getROImask(imHandle,roidata1.patchHandle);
+roidata0 = getROIdata(imHandle,mask0);
+roidata1 = getROIdata(imHandle,mask1);
 cdata = imHandle.CData;
 [H,P,CI] = ttest2(cdata(mask0),cdata(mask1));
 Comparison.fgROI = imHandle.UserData.ROI(index0).Name;
 Comparison.bgROI = imHandle.UserData.ROI(index1).Name;
+Comparison.ratio = roidata0.mean/roidata1.mean;
 Comparison.contrast = (roidata0.mean-roidata1.mean)/roidata1.mean;
 Comparison.std_joint = sqrt((roidata0.N*(roidata0.std^2)+roidata1.N*(roidata1.std^2))/(roidata0.N+roidata1.N));
-Comparison.CNR = Comparison.contrast/Comparison.std_joint;
-Comparison.TTest_UnequalMeans = H;
-Comparison.TTest_PValue = P;
+Comparison.CNR = abs(roidata0.mean-roidata1.mean)/Comparison.std_joint;
+Comparison.TTest_H = H;
+Comparison.TTest_P = P;
 Comparison.TTest_DiffMeansCI = CI;
-fprintf('Comparison:\n');
-disp(Comparison);
-imHandle.UserData.Comparison = Comparison;
+end
+
+function C = comp2cell(COMP)
+    C = [fieldnames(COMP) struct2cell(COMP)];
+    C = C(1:8,:);
 end
 
 function renameROI(hObject,eventdata)
@@ -179,6 +269,7 @@ roidata = getROIdata(imHandle,mask);
 roidata.Name = sprintf('ROI %g',index);
 roidata.patchHandle = patchHandle;
 roidata.imHandle = imHandle;
+roidata.statTable = [];
 if index == 1;
     imHandle.UserData.ROI = roidata;
 else
@@ -195,6 +286,7 @@ end
 function roidata = getROIdata(imHandle,mask)
 cData = get(imHandle,'cData');
 c = cData(mask);
+roidata.Name = '';
 roidata.N = numel(c);
 roidata.mean = nanmean(c);
 roidata.std = nanstd(c);
