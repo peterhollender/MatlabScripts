6cd03810c2190d7f5dfb960192180155625943d9
Peter Hollender <peterhollender@gmail.com>
2016-03-16 15:32:33 -0400

    improve looks for pathdef.m

--- include.m
+++ include.m
@@ -2,5 +2,10 @@ function include(package_name,git_path)
 if ~exist('git_path','var')
     git_path = fileparts(fileparts(mfilename('fullpath')));
 end
-cmd = sprintf('addpath(genpath(''%s''));',fullfile(git_path,package_name));
-evalin('caller',cmd);
+path_def_file = fullfile(git_path,package_name,'pathdef.m');
+if exist(path_def_file,'file')
+    evalin('caller',sprintf('run %s;',path_def_file));
+else
+    cmd = sprintf('addpath(genpath(''%s''));',fullfile(git_path,package_name));
+    evalin('caller',cmd);
+end
