fbc920e52e9e54924d1c6021ebe3e008e9e6ddc1
Peter Hollender <pjh7@duke.edu>
2018-03-27 11:52:28 -0400

    ProgressBar ready to go

--- ProgressBar.m
+++ ProgressBar.m
@@ -5,50 +5,70 @@ classdef ProgressBar < handle
         time_remaining = NaN;
         elapsed_time = 0;
         as_text = false;
-        as_waitbar = true;
+        as_waitbar = true;        
+        msg = '';        
+
     end
     properties (Hidden=true)
-        msg = '';
-        buffer = [0];
+        buffer_size = 0;        
         min_buffer = 1;
-        buffer_size = 0;
-        waitbar_handle = [];
+        buffer = [0];
+        waitbar_handle = NaN;
         start_time = NaN;
-        last_time = NaN;
+        last_time = 0;
         last_msg = '';
     end
     methods
-        function self = ProgressBar(N, msg)
-            self.N = N; 
-            if exist('msg','var')
-                self.msg = msg;
-            end
-            self.waitbar_handle = waitbar(0,self.get_msg); 
-            self.reset
+        function self = ProgressBar(N, varargin)
+            %PROGRESSBAR Interactive Progress Bar
+            % obj = PROGRESSBAR(N, varargin)
+            %
+            %PROGRESSBAR creates a text or graphical progress bar, which is
+            % updated by calling the next() method.
+            %
+            %PROGRESSBAR(N, 'p1', v1, ...) sets the properties of
+            % the objec.
+            %
+            %INPUT PROPERTIES:
+            %   char msg: the string to be displayed
+            %   boolean as_text: show a text based progress bar
+            %   boolean as_waitbar: show a graphical waitbar
+            %
+            %QUERYABLE PROPERTIES:
+            %   double time_remaining: the number of seconds until the loop
+            %    is expected to finish
+            %   double elapsed_time: the amount of time elapsed in seconds
+            %   int index: the current index
+            %   int N: the total number of loops
+            %
+            %
+            %METHODS:
+            %   obj.reset - reset the counter and restart the clock
+            %   obj.next - increment the counter and update the display
+            %   obj.close - close the progress bar 
+            %
+            
+            self.N = N;
+            self = setprops(self, varargin);
         end
         function reset(self)
-            self.index = 0;
-            self.start_time = NaN;
-            self.last_time = 0;
-            self.elapsed_time = 0;
+            % Resets the Progress bar and restarts the clock
+            % Called automatically by next() when index == 0
             if self.buffer_size > 0
                 self.buffer = nan(self.buffer_size,1);
             end
-            self.update;
-        end
-        function start(self)
             self.start_time = tic;
             self.index = 1;
+            self.last_time = 0;
+            self.elapsed_time = 0;
             self.update;
         end
-        function msg = get_msg(self)
-            msg = sprintf('%s [%d/%d] %s/%s', self.msg, self.index,...
-                self.N, self.time2clock(self.elapsed_time),...
-                self.time2clock(self.time_remaining+self.elapsed_time));
-        end
+
         function next(self)
+            % Increment the index and update the display. This should be
+            % called at the *beginning* of each loop. 
             if self.index == 0
-                self.start
+                self.reset
             else
                 new_time = toc(self.start_time);
                 if self.buffer_size > 0
@@ -63,22 +83,8 @@ classdef ProgressBar < handle
                 end
             end
         end
-        function t_avg = time_per_inc(self)
-            if self.index <= self.min_buffer
-                t_avg = NaN;
-            else
-                if self.buffer_size > 0
-                    t_avg = nanmean(self.buffer);
-                else
-                    t_avg = self.elapsed_time / (self.index - 1);
-                end
-            end
-        end
-        function t_remain = estimate_remaining_time(self)
-            t_avg = self.time_per_inc;
-            t_remain = (self.N-(self.index-1)) * t_avg;
-        end
         function close(self)
+            % Closes the progress bar
             if ishandle(self.waitbar_handle)
                 close(self.waitbar_handle)
             end
@@ -86,10 +92,8 @@ classdef ProgressBar < handle
                 fprintf('\n')
             end
         end
-        function update(self)
-            if isnan(self.start_time)
-                return
-            end
+    function update(self)
+        % Update the display of the progress bar
             self.elapsed_time = toc(self.start_time);
             self.time_remaining = self.estimate_remaining_time;
             new_msg = self.get_msg;
@@ -109,6 +113,30 @@ classdef ProgressBar < handle
                 self.last_msg = new_msg;               
             end
         end
+    end
+
+    methods (Hidden = true)
+        function msg = get_msg(self)
+            msg = sprintf('%s [%d/%d] %s/%s', self.msg, self.index,...
+                self.N, self.time2clock(self.elapsed_time),...
+                self.time2clock(self.time_remaining+self.elapsed_time));
+        end
+        function t_avg = time_per_inc(self)
+            if self.index <= self.min_buffer
+                t_avg = NaN;
+            else
+                if self.buffer_size > 0
+                    t_avg = nanmean(self.buffer);
+                else
+                    t_avg = self.elapsed_time / (self.index - 1);
+                end
+            end
+        end
+        function t_remain = estimate_remaining_time(self)
+            t_avg = self.time_per_inc;
+            t_remain = (self.N-(self.index-1)) * t_avg;
+        end
+        
     end
     methods (Static)
         function clockstr = time2clock(time)
